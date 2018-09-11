14cfb56e81595bac6eeed2833f5aa1603107704e
undancer <undancer@gmail.com>
2018-09-12 01:23:13 +0800

    添加redux实现

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,24 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux';
+import {enthusiasm} from './reducers/index';
+import {StoreState} from './types/index';
+import {Provider} from 'react-redux';
+import {EnthusiasmAction} from "./actions";
+
+
+const store = createStore<StoreState, EnthusiasmAction, any, any>(enthusiasm, {
+    enthusiasmLevel: 1,
+    languageName: 'TypeScript',
+});
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+    (
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    ),
+    document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
